07b2453661e3c2d5b6ed493a1f86543b2acb824d
Vishal Arora <varora1975@gmail.com>
2020-06-12 17:08:23 +1200

    Updates

---
+++ precache-manifest.60bcfbdf30b3812061bd1b93a032eb33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2047bed8eecef4f0f8a16abeec1fc7e6",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "183bbc675ff958ab5cae",
+    "url": "/react-tutorial/static/css/main.b044d04a.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/react-tutorial/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183bbc675ff958ab5cae",
+    "url": "/react-tutorial/static/js/main.65ffa6b5.chunk.js"
+  },
+  {
+    "revision": "09acd601ee317c35e699",
+    "url": "/react-tutorial/static/js/runtime-main.9063aec8.js"
+  }
+]);
